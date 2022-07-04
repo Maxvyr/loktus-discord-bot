b6ad2ceccc98310b329b90850927558ab8edb0ca
maxvyr <maxvid30@gmail.com>
2022-07-04 22:47:09 +0200

    🎉 first commit

---
+++ src/bot.ts
@@ -0,0 +1,16 @@
+import { Client } from 'discord.js';
+import interactionCreate from './listeners/interactionCreate';
+import ready from './listeners/ready';
+import dotenv from 'dotenv';
+
+dotenv.config();
+console.log("Bot is starting...");
+
+const client = new Client({
+    intents: []
+});
+
+ready(client);
+interactionCreate(client);
+client.login(process.env.BOT_TOKEN)
+// console.log(client);
