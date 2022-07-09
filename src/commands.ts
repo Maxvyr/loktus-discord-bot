502bc6353f3d20de40171bad95a663d34f85bf36
maxvyr <maxvid30@gmail.com>
2022-07-09 10:56:02 +0200

    🚧 start add new command

--- src/commands.ts
+++ src/commands.ts
@@ -1,4 +1,5 @@
 import { Command } from "./command";
+import { Games } from "./commands/games";
 import { Hello } from "./commands/hello";
 
-export const Commands: Command[] = [Hello];
+export const Commands: Command[] = [Hello, Games];
